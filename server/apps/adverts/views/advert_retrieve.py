83ba34cfa308935ff86f37dd1246374360503fe8
raisultan <ki.xbozz@gmail.com>
2021-05-14 13:19:40 +0700

    updated

--- server/apps/adverts/views/advert_retrieve.py
+++ server/apps/adverts/views/advert_retrieve.py
@@ -1,7 +1,17 @@
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
 
 from ..serializers import AdvertSerializer
+from ..services import AdvertViewCountService
 
 
 class AdvertRetrieveAPIView(RetrieveAPIView):
     serializer_class = AdvertSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        advert = self.get_object()
+        serializer = self.get_serializer(advert)
+        resp = Response(serializer.data)
+
+        AdvertViewCountService.count(request, advert)
+        return resp
