48a4f4747447ce2e4cf2873909c5ea33de093212
raisultan <ki.xbozz@gmail.com>
2021-05-14 13:17:27 +0700

    viewcount service: raw

---
+++ server/apps/adverts/services/__init__.py
@@ -0,0 +1,1 @@
+from .advert_viewcount import AdvertViewCountService
---
+++ server/apps/adverts/services/advert_viewcount.py
@@ -0,0 +1,64 @@
+from typing import Final
+import re
+from django.core.cache import cache
+
+from django.http import HttpRequest
+
+from ..models import Advert
+
+
+class AdvertViewCountService:
+    """
+    Mixin to evaluate a HttpRequest and a HitCount and determine whether or not
+    the HitCount should be incremented and the Hit recorded.
+    """
+
+    DEFAULT_IP: Final[str] = '127.0.0.1'
+    FALLBACK_IP: Final[str] = '10.0.0.1'
+    IP_REGEX: Final[str] = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+
+    VIEW_COUNT_CACHE_TEMPLATE: Final[str] = 'viewcount-{ip}-{user_agent}-{session_key}'
+    REQUEST_SESSION_CACHE_LIFETIME: Final[int] = 60 * 60  # 1 hour
+
+    @classmethod
+    def count(cls, request: HttpRequest, advert: Advert) -> bool:
+        request_cache_key = cls._compose_cache_key(request)
+        request_cache_value = cache.get(request_cache_key)
+
+        if not request_cache_value:
+            advert.views = advert.views + 1
+            advert.save()
+
+            is_counted = True
+        else:
+            cache.set(request_cache_key, True, timeout=cls.REQUEST_SESSION_CACHE_LIFETIME)
+            is_counted = False
+
+        return is_counted
+
+    @classmethod
+    def _get_ip(cls, request: HttpRequest) -> str:
+        ip_address = request.META.get(
+            'HTTP_X_FORWARDED_FOR',
+            request.META.get('REMOTE_ADDR', cls.DEFAULT_IP),
+        )
+
+        if ip_address:
+            try:
+                ip_address = cls.IP_RE.match(ip_address)
+                if ip_address:
+                    ip_address = ip_address.group(0)
+                else:
+                    ip_address = cls.FALLBACK_IP
+            except IndexError:
+                pass
+
+        return ip_address
+
+    @classmethod
+    def _compose_cache_key(cls, request: HttpRequest) -> str:
+        return cls.VIEW_COUNT_CACHE_TEMPLATE.format(
+            ip=cls._get_ip(request),
+            user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],
+            session_key=request.session.session_key,
+        )
